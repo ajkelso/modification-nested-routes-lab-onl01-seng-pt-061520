ff9b8f093b7feb831d09f3b44f2fa6c10fa85474
ajkelso <ajkelso@gmail.com>
2020-11-16 12:44:04 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+    def artist_id_field(song)
+        if song.artist.nil?
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        end
+    end
+
+
 end
